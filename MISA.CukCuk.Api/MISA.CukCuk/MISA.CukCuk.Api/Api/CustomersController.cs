36d0a47cf45d3c8d9cb2c71a098f0da929bfe6e4
phungdactai <phungdactai98@gmail.com>
2021-01-08 13:34:00 +0700

    add pai

---
+++ MISA.CukCuk.Api/MISA.CukCuk/MISA.CukCuk.Api/Api/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.CukCuk.Api.Models;
+
+namespace MISA.CukCuk.Api.Api
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customer = new Customer();
+            
+            return StatusCode(200, customer.listCustomers());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer) { 
+            return StatusCode(200, customer.addCustomer());
+        }
+
+        [HttpGet("{id}/{name}")]
+        public string Get(int id, string name)
+        {
+            return "name";
+        }
+        [HttpPut]
+        public string Put()
+        {
+            return "c";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "d";
+        }
+    }
+}
---
+++ MISA.CukCuk.Api/MISA.CukCuk/MISA.CukCuk.Api/Models/Customer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Api.Models
+{
+    /// <summary>
+    /// Khách hàng
+    /// </summary>
+    /// CreatedBy: PDTAI (8/1/2021)
+    public class Customer
+    {
+        #region Declare
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+
+        /// <summary>
+        /// Họ và tên đầy đủ
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Số thẻ thành viên
+        /// </summary>
+        public string MemberCardCode { get; set; }
+
+        /// <summary>
+        /// Khóa chính nhóm khách hàng
+        /// </summary>
+        public Guid CustomerIdGroup { get; set; }
+
+        /// <summary>
+        /// Ngày sinh
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Giới tính 0-Nữ, 1-Nam, 2-Khác
+        /// </summary>
+        public int Gender { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Tên công ty
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Mã số thuế công ty
+        /// </summary>
+        public string CompanyTaxCode { get; set; }
+
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+
+        /// <summary>
+        /// Người tạo
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Nhày chỉnh sửa gần nhất
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Người chỉnh sửa gần nhất
+        /// </summary>
+        public string ModifiedBy { get; set; }
+        #endregion
+
+        #region Method
+        public List<Customer> listCustomers(){
+            var customers = new List<Customer>();
+            for (int i = 0; i < 10; i++)
+            {
+                var customer = new Customer();
+                customer.CustomerId = Guid.NewGuid();
+                customer.CustomerCode = $"KH000{i + 1}";
+                customer.FullName = $"Name{i + 1}";
+                customers.Add(customer);
+            }
+            return customers;
+        }
+
+        public Customer addCustomer()
+        {
+            var customers = new List<Customer>();
+
+            return customers;
+        }
+        #endregion
+    }
+}
